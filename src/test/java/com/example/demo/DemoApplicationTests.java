16fb801435736c12cabf75b5d7950e603acfbb3b
chubbyhippo <mkmsacc@outlook.com>
2024-11-24 19:33:50 +0700

    Add display name to test method
    Annotated the `shouldReturnHelloUsingWebTestClient` test with `@DisplayName` for better readability in test reports. This change helps in identifying the purpose of the test more clearly during execution.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class DemoApplicationTests {
 
     @Test
+    @DisplayName("should return hello using web test client")
     void shouldReturnHelloUsingWebTestClient(@Autowired WebTestClient webTestClient) {
         var responseBody = webTestClient.get()
                 .uri("/")
