9f34ca3d4885b4c7ca12f76c164b3bc2c14ec86c
chubbyhippo <mkmsacc@outlook.com>
2024-11-24 19:38:20 +0700

    Add test case using MockMvcTester
    Implemented a new test case that checks the "Hello" endpoint using MockMvcTester, enhancing testing coverage. This complements existing tests with WebTestClient for more robust verification of application endpoints.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,13 +3,33 @@ package com.example.demo;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+
+import java.io.UnsupportedEncodingException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class DemoApplicationTests {
+
+    @Test
+    @DisplayName("should return hello using mock mvc tester")
+    void shouldReturnHelloUsingMockMvcTester(@Autowired MockMvcTester mockMvcTester) throws UnsupportedEncodingException {
+        var response = mockMvcTester.get()
+                .uri("/")
+                .exchange()
+                .getMvcResult()
+                .getResponse();
+
+        var contentAsString = response.getContentAsString();
+
+        assertThat(contentAsString)
+                .isEqualTo("Hello");
+    }
 
     @Test
     @DisplayName("should return hello using web test client")
